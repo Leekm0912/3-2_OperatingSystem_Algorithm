d88f95abe3a6d30b16671219dab9cbe4211edf02
leekm <skatjrdndqo@naver.com>
2021-11-29 15:32:24 +0900

    commit

---
+++ chap06_ReaderWriter_using_interrupt/Writer.java
@@ -0,0 +1,18 @@
+
+
+public class Writer extends Thread{
+	private Moniter m; 	// 스레드 간 공유 객체 ReadWriteBuffer
+
+	public Writer(Moniter m, int num) {
+		super("Writer-" + Integer.toString(num));
+		this.m = m;
+	}
+	
+	@Override
+	public void run() {
+		m.beginWrite();
+		m.endWrite();
+	}
+}
+
+
