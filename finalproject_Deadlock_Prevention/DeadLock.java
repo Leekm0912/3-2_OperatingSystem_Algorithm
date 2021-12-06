5d3ece4f5a237c1f68bdb8d86d3e5c71c79bfb3a
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-12-06 17:33:22 +0900

    코드정리 및 시간측정 부분 추가

--- finalproject_Deadlock_Prevention/DeadLock.java
+++ finalproject_Deadlock_Prevention/DeadLock.java
@@ -4,33 +4,51 @@ import java.util.*;
 import java.util.concurrent.Semaphore;
 
 public class DeadLock extends Thread {
-	int id;
-	Random r = new Random();
-	int require;
-	List<Semaphore> resource;
-	Map<Integer, Semaphore> save = new HashMap<>();
-	int numOfResource;
+	private int id;
+	private int numOfResource;
+	private int maxResource;
+	private int require = 0;
+	private Random random = new Random();
+	private List<Semaphore> resource;
+	private Map<Integer, Semaphore> save = new HashMap<>();
+	private int loop = -1;
+	public boolean end = false;
+	private int sleepTime = 0;
 
-	public DeadLock(int id, int n, List<Semaphore> resource) {
+	public DeadLock(int id, int numOfResource, List<Semaphore> resource, int maxResource, int sleepTime) {
 		this.id = id;
 		this.resource = resource;
-		this.numOfResource = n;
+		this.numOfResource = numOfResource;
+		this.maxResource = maxResource;
+		this.sleepTime = sleepTime;
+	}
+
+	public DeadLock(int id, int numOfResource, List<Semaphore> resource, int maxResource, int sleepTime,
+			int loopCount) {
+		this(id, numOfResource, resource, maxResource, sleepTime);
+		this.loop = loopCount;
 	}
 
 	@Override
 	public void run() {
-		while(true) {
-			int rand = r.nextInt(5) + 1; // 몇개의 자원을 사용할지. 1~5개의 자원을 사용함.
-			// 고른 자원을 save에 추가
-			while (save.size() < rand) {
-				int temp = r.nextInt(this.numOfResource);
-				if (!save.containsKey(temp)) {
-					save.put(temp, this.resource.get(temp));
-				}
-			}
-			System.out.println("id " + this.id + " need : " + Arrays.toString(save.keySet().toArray()));
-
+		// loop가 -1이 아니면 무한반복 안하도록 설정.
+		int loopCount = 0;
+		if (loop != -1) {
+			loopCount = loop;
+		}
+		while (loopCount > 0 || this.loop == -1) {
 			try {
+				// 스레드가 몇개의 자원을 필요로 할지. 1 ~ maxResource 사이의 값이 랜덤으로 사용됨.
+				int rand = random.nextInt(this.maxResource) + 1;
+				// 고른 자원을 save에 추가
+				while (save.size() < rand) {
+					int temp = random.nextInt(this.numOfResource);
+					if (!save.containsKey(temp)) {
+						save.put(temp, this.resource.get(temp));
+					}
+				}
+				System.out.println("id " + this.id + " need : " + Arrays.toString(save.keySet().toArray()));
+
 				while (require < rand) {
 					this.save.forEach((k, v) -> {
 						if (v.tryAcquire()) {
@@ -51,23 +69,54 @@ public class DeadLock extends Thread {
 				e.printStackTrace();
 			}
 		}
+		this.end = true;
 	}
 
 	private void work(Set<Integer> need) throws InterruptedException {
 		System.out.println("work : " + this.id + " need : " + Arrays.toString(need.toArray()));
-		sleep(1000);
+		sleep(this.sleepTime);
 	}
 
 	public static void main(String[] args) {
-		int n = 10;
-		List<Semaphore> stick = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			stick.add(new Semaphore(1));
+		// 리소스의 개수
+		int numOfResource = 20;
+		// 스레드의 개수
+		int numOfThread = 15;
+		// 스레드가 필요로 하는 최대 리소스 개수
+		int maxResource = 5;
+		// 반복 횟수. -1이면 무한반복
+		int loop = -1;
+		// work의 sleep 시간
+		int sleepTime = 1000;
+
+		List<Semaphore> resource = new ArrayList<>();
+		for (int i = 0; i < numOfResource; i++) {
+			resource.add(new Semaphore(1));
 		}
-//        Semaphore ls = new Semaphore(1);
-		for (int i = 0; i < n; i++) {
-			DeadLock p = new DeadLock(i, n, stick);
+
+		long beforeTime = System.currentTimeMillis(); // 시작시간 측정
+		List<DeadLock> saveThread = new ArrayList<DeadLock>();
+		for (int i = 0; i < numOfThread; i++) {
+			DeadLock p = new DeadLock(i, // Thread id
+					numOfResource, resource, maxResource, sleepTime, loop);
 			p.start();
+			saveThread.add(p);
 		}
+
+		// thread가 끝날때까지 대기.
+		while (true) {
+			boolean threadEnd = true;
+			for (int i = 0; i < saveThread.size(); i++) {
+				DeadLock temp = saveThread.get(i);
+				if (temp.end == false) {
+					threadEnd = false;
+				}
+			}
+			if (threadEnd) {
+				break;
+			}
+		}
+		long afterTime = System.currentTimeMillis(); // 코드 실행 후의 시간 측정
+		System.err.println(" 소요시간(ms) : " + (afterTime - beforeTime));
 	}
 }
