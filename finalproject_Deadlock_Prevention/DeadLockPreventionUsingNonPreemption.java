99f2eb5b74e3ba0e5ef678d681aba20168e27a16
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-12-07 12:29:48 +0900

    주석 추가

--- finalproject_Deadlock_Prevention/DeadLockPreventionUsingNonPreemption.java
+++ finalproject_Deadlock_Prevention/DeadLockPreventionUsingNonPreemption.java
@@ -10,17 +10,17 @@ import java.util.Set;
 import java.util.concurrent.Semaphore;
 
 public class DeadLockPreventionUsingNonPreemption extends Thread {
-	private int id;
-	private int numOfResource;
-	private int maxResource;
-	private int require = 0;
+	private int id; // 스레드의 id
+	private int numOfResource; // 공유 자원의 개수
+	private int maxResource; // 스레드가 필요로하는 자원의 최대 개수 (1 ~ maxResource개 랜덤 요구함)
+	private int require = 0; // 스레드가 필요로 하는 자원을 몇개 가져왔는지. 
 	private Random random = new Random();
-	private List<Semaphore> resource;
-	private Map<Integer, Semaphore> save = new HashMap<>();
-	private boolean[] lock;
-	private int loop = -1;
-	public boolean end = false;
-	private int sleepTime = 0;
+	private List<Semaphore> resource; // 모든 공유자원을 저장해놓은 리스트.
+	private Map<Integer, Semaphore> save = new HashMap<>(); // 자신이 가져온 공유자원을 저장한 리스트
+	private boolean[] lock; // 공유 자원이 작업중인지 판단하기 위한 배열.
+	private int loop = -1; // 몇회 반복할 것인지. -1이면 무한반복
+	public boolean end = false; // 스레드의 작업이 끝났는지.
+	private int sleepTime = 0; // work 메서드가 얼마동안 작업(현재는 sleep)을 할지.
 
 	public DeadLockPreventionUsingNonPreemption(int id, int numOfResource, List<Semaphore> resource, int maxResource,
 			int sleepTime) {
@@ -45,6 +45,7 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 		if (loop != -1) {
 			loopCount = loop;
 		}
+
 		while (loopCount > 0 || this.loop == -1) {
 			// System.out.println("start "+ id);
 			// 스레드가 몇개의 자원을 필요로 할지. 1 ~ maxResource 사이의 값이 랜덤으로 사용됨.
@@ -59,6 +60,7 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 			System.out.println("id " + this.id + " need : " + Arrays.toString(this.save.keySet().toArray()));
 
 			try {
+				// 필요 자원을 모두 가져올동안 반복함.
 				while (require < rand) {
 					this.save.forEach((k, v) -> {
 						if (v.tryAcquire()) {
@@ -86,6 +88,7 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 					});
 					// System.out.println(require);
 				}
+				// 반복문을 빠져나왔으면 필요 자원을 모두 가져온것. 작업 수행하면 됨.
 				work(this.save.keySet());
 				// 작업 후 초기화
 				for (Semaphore s : this.save.values()) {
@@ -99,15 +102,20 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 				e.printStackTrace();
 			}
 		}
+		// 스레드의 작업이 끝나면 true로 바꿔 작업이 끝난걸 알려줌.
 		this.end = true;
 	}
 
 	private void work(Set<Integer> need) throws InterruptedException {
+		// 작업 중일때는 사용중인 자원에 lock을 걸어 선점이 불가능하게 함.
 		this.save.forEach((k, v) -> {
 			this.lock[k] = true;
 		});
+		// 작업 수행
 		System.out.println("work : " + this.id + " use : " + Arrays.toString(need.toArray()));
 		sleep(this.sleepTime);
+		// 작업 수행
+		// 작업이 끝나면 lock을 풀어주어 선점이 가능하게 함.
 		this.save.forEach((k, v) -> {
 			this.lock[k] = false;
 		});
@@ -124,7 +132,7 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 		int loop = -1;
 		// work의 sleep 시간
 		int sleepTime = 1000;
-
+		
 		List<Semaphore> resource = new ArrayList<>();
 		for (int i = 0; i < numOfResource; i++) {
 			resource.add(new Semaphore(1));
@@ -132,6 +140,7 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 
 		long beforeTime = System.currentTimeMillis(); // 시작시간 측정
 		List<DeadLockPreventionUsingNonPreemption> saveThread = new ArrayList<DeadLockPreventionUsingNonPreemption>();
+		// 스레드 생성 및 실행
 		for (int i = 0; i < numOfThread; i++) {
 			DeadLockPreventionUsingNonPreemption p = new DeadLockPreventionUsingNonPreemption(
 					i, // Thread id
@@ -140,15 +149,17 @@ public class DeadLockPreventionUsingNonPreemption extends Thread {
 			p.start();
 			saveThread.add(p);
 		}
-
+		// 자식 스레드 작업이 종료될때까지 메인 스레드 대기.
 		while (true) {
 			boolean threadEnd = true;
 			for (int i = 0; i < saveThread.size(); i++) {
 				DeadLockPreventionUsingNonPreemption temp = saveThread.get(i);
+				// 자식 스레드가 하나라도 작업이 종료되지 않으면 반복문 탈출 안함.
 				if (temp.end == false) {
 					threadEnd = false;
 				}
 			}
+			// 자식 스레드의 작업이 끝나면 반복문 탈출.
 			if (threadEnd) {
 				break;
 			}
